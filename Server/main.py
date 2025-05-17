970110d000e7c7d3a48030d636fc5ecc25775826
AdvancedK_ <M1150562056@outlook.com>
2025-05-17 10:23:47 +0800

    结工

--- Server/main.py
+++ Server/main.py
@@ -1,10 +1,13 @@
 from Server.apps import api
 from Server.core.LatexamServer import LatexamServer
+from yaml import load, Loader
 
 
-server = LatexamServer()
+with open("options.yaml", "r") as f:
+    config = load(f, Loader=Loader)
+server = LatexamServer(admin_password=config["server"]["admin_pass"])
 
 
 if __name__ == "__main__":
     server.app.include_router(api)
-    server.run()
+    server.run(host=config["server"]["host"], port=config["server"]["port"])
